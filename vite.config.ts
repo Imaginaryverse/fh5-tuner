5b7538c8528edec0e9492e226865efe0f7e00473
Imagineryverse <bertilsson.anton@gmail.com>
2023-07-21 21:35:51 +0200

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/fh5-tuner/',
   plugins: [react()],
-})
+});
